3433140e4899565dd2a7a2d7af8d0085b1091b46
zecosta <josepirescosta44.jc68@gmail.com>
2023-10-16 12:28:25 +0100

    waitForReply e serverReply

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import sys
 import socket
 import pickle
+import select
 
 """
 @author: Catarina Gonçalves Costa | 62497
@@ -10,20 +11,38 @@ import pickle
 host_of_server = sys.argv[1]
 portSP = int(sys.argv[2])
 fileName = sys.argv[3]
-chunkSize = sys.argv[4]
+chunkSize = int(sys.argv[4])
 
 serverAddressPort = ("127.0.0.1", portSP)
 
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 
+def waitForReply(UDPClientSocket):
+    rx, tx, er = select.select([UDPClientSocket], [], [], 1)
+    if rx == []:
+        return False
+    else:
+        return True
+
+
 def main():
-    file = open(fileName, "wb")
+    file = open(
+        fileName, "wb"
+    )  # Cria um ficheiro com o mesmo nome do ficheiro do server
     offset = 0
     while True:
-        request = (fileName, offset)  # Falta blockSize
+        request = (fileName, offset, chunkSize)
         req = pickle.dumps(request)
         UDPClientSocket.sendto(req, serverAddressPort)
+        if waitForReply(UDPClientSocket):
+            datagram = UDPClientSocket.recvfrom()  # meter tamanho do datagram
+
+            # file.write(fileChunk)
+            if offset:
+                break
+            else:
+                offset += chunkSize
 
 
 main()
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import sys
 import socket
 import pickle
 import os
+import random
 
 """
 @author: Catarina Gonçalves Costa | 62497
@@ -12,12 +13,20 @@ localIP = "127.0.0.1"
 portSP = sys.argv[1]
 bufferSize = 1024
 status = -1
+# cOffset = 0
 
 # Create a datagram socket
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 # Bind to address and ip
 UDPServerSocket.bind((localIP, portSP))
+
+
+def serverReply(msgP, UDPServerSocket, address):
+    randomNumber = random.randint(0, 10)
+    if randomNumber >= 3:
+        UDPServerSocket.sendto(msgP, address)
+    return
 
 
 def main():
@@ -27,18 +36,20 @@ def main():
         request = pickle.loads(message)
         fileName = request[0]
         offset = request[1]
-        noBytes = request[2]
-        if not os.path.isfile(fileName):
+        chunkSize = request[2]
+        if not os.path.isfile(fileName):  # Ficheiro não existe no server
             status = 1
-        elif (noBytes * offset) >= os.path.getsize(fileName):  # VERIFICAR
+        elif chunkSize > os.path.getsize(fileName):  # VERIFICAR
             status = 2
         else:
             status = 0
         file = open(fileName, "r")
         file.seek(offset)
-        data = file.read(noBytes) # Informação lida
-        # bytesRead = os.path.getsize(fileName) - noBytes * offset
-        # reply = (0, data, )
+        data = file.read(chunkSize)  # Informação lida
+        bytesRead = os.path.getsize(fileName) - offset # VERIFICAR
+        msg = (status, bytesRead, data)
+        msgP = pickle.dumps(msg)
+        serverReply(msgP, UDPServerSocket, address)
 
 
 main()
