a6f3b37685463a3650097d1bd4527a49371bceeb
zecosta <josepirescosta44.jc68@gmail.com>
2023-11-04 19:30:25 +0000

    call Sábado

--- receiver.py
+++ receiver.py
@@ -12,12 +12,12 @@ bufferSize = 1024
 # Create a datagram socket
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-# Bind to address and ip
-UDPServerSocket.bind((receiverIP, receiverPort))
+# # Bind to address and ip
+# UDPServerSocket.bind((receiverIP, receiverPort))
 
 def waitForReply( uSocket, timeOutInSeconds ): 
     rx, tx, er = select.select( [uSocket], [], [], timeOutInSeconds) 
-# waits for data or timeout 
+    # waits for data or timeout 
     if rx == []: 
         return False 
     else: 
@@ -25,10 +25,10 @@ def waitForReply( uSocket, timeOutInSeconds ):
 
 
 def sendDatagram (msg, sock, address): 
-# msg is a byte array ready to be sent 
-# Generate random number in the range of 1 to 10 
+    # msg is a byte array ready to be sent 
+    # Generate random number in the range of 1 to 10 
     rand = random.randint(1, 10) 
- # If rand is less is than 3, do not respond (20% of loss probability) 
+    # If rand is less is than 3, do not respond (20% of loss probability) 
     if rand >= 3: 
         sock.sendto(msg, address) 
 
--- sender.py
+++ sender.py
@@ -11,34 +11,93 @@ receiverPort = int(sys.argv[4])
 filename = sys.argv[5]
 windowSizeInBlocks = int(sys.argv[6])
 
+base = 1
+nextSeqNum = 1
+chunkSize = 1024
+packets = []
+
+class cStates:
+    INITIAL_STATE
+    
 recieverAddressPort = (receiverIP, receiverPort)
 
 UDPSenderSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
+# # Bind to address and ip
+# UDPSenderSocket.bind((receiverIP, receiverPort))
+
 def waitForReply( uSocket, timeOutInSeconds ): 
     rx, tx, er = select.select( [uSocket], [], [], timeOutInSeconds) 
-# waits for data or timeout 
+    # waits for data or timeout 
     if rx == []: 
         return False 
     else: 
         return True 
     
 def sendDatagram (msg, sock, address): 
-# msg is a byte array ready to be sent 
-# Generate random number in the range of 1 to 10 
+    # msg is a byte array ready to be sent 
+    # Generate random number in the range of 1 to 10 
     rand = random.randint(1, 10) 
- # If rand is less is than 3, do not respond (20% of loss probability) 
+    # If rand is less is than 3, do not respond (20% of loss probability) 
     if rand >= 3: 
         sock.sendto(msg, address) 
 
-    
-def main():
-    file = open(filename, "rb")
-    msgSend = (0, nSeq, data)
-    msgSendP = pickle.dumps(msgSend)
-    sendDatagram(msgSendP, UDPSenderSocket, recieverAddressPort)
+def recv_ack(data):        
+       msgRecv = pickle.loads(data)
+       base = msgRecv[1]
+       if(base >= nextSeqNum): # o package foi recebido
+           stop_timer
+        else: # o package foi perdido a enviar/ voltar a enviar
+            start_timer
 
-    if waitForReply(UDPSenderSocket):
+
+def rdt_sent(data):
+    if(nextSeqNum < base + windowSizeInBlocks):
+        msgSendP = pickle.dumps((nextSeqNum,data))
+        sendDatagram(msgSendP, UDPSenderSocket, recieverAddressPort)
+        if(base==nextSeqNum):
+            start_timer
+        nextSeqNum+=1
+       # recv_ack()
+
+
+def main():
+    global base
+    global nextSeqNum
+    file = open(filename, "rb")
+    seqN = 0
+    while True:
+        data = file.read(chunkSize)
+        if not data:
+            break  # End of file
+        packet = pickle.dumps((seqN, data))
+        packets.append(packet)
+        seqN += 1
+
+    state = cStates.INITIAL_STATE 
+    while state != FINAL_STATE: 
+        match state:
+            case cStates.INITIAL_STATE:
+                base = 1
+                nextSeqNum = 1
+                break
+            case cStates.STATE_1: # enviar packets
+                offset = chunkSize * base # VERFICAR SE É A MULTIPLIA PELA base OU nextSeqNum
+                file.seek(offset)
+                data = file.read(chunkSize)  # Informação lida
+                if not data:
+                    break
+                rdt_sent(data)
+                state = STATE_2
+                break
+            case cStates.STATE_2: #receber ack
+                recv_ack()
+                break
+            case cStates.STATE_3: #timeout
+                break
+
+
+
 
 
 
