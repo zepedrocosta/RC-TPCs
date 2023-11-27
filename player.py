2a7660142ed6f6005e6797d016b3b8a76c3a0d63
zepedrocosta <josepirescosta44.jc68@gmail.com>
2023-11-27 16:21:51 +0000

    FINAL

--- player.py
+++ player.py
@@ -2,6 +2,11 @@ import socket
 import subprocess
 import os
 
+"""
+@author: Catarina Gonçalves Costa | 62497
+@author: José Pedro Pires Costa | 62637
+"""
+
 # Start a socket listening for connections on 0.0.0.0:8000
 # (0.0.0.0 means all interfaces)
 server_socket = socket.socket()
@@ -11,12 +16,7 @@ server_socket.listen(0)
 connection = server_socket.accept()[0].makefile("rb")
 
 try:
-    # Run a viewer with an appropriate command line. Uncomment the mplayer
-    # version if you would prefer to use mplayer instead of VLC
-    # For Mac: alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
-    # cmdline = '/Applications/VLC.app/Contents/MacOS/VLC --demux h264 -'
-
-    cmdline = "C:/Users/josep/Downloads/MPlayer-generic-r38407+g10a56363a7/MPlayer-generic-r38407+g10a56363a7/mplayer.exe -ni -"
+    cmdline = "MPlayer-generic-r38407+g10a56363a7/mplayer.exe -ni -"  # Pasta na directoria de trabalho
     player = subprocess.Popen(cmdline.split(), stdin=subprocess.PIPE)
     while True:
         # Repeatedly read 1k of data from the connection and write it to
--- proxy.py
+++ proxy.py
@@ -10,10 +10,6 @@ import linecache
 @author: José Pedro Pires Costa | 62637
 """
 
-baseURL = sys.argv[1]
-movieName = sys.argv[2]
-track = int(sys.argv[3])
-
 playerAddressPort = ("localhost", 8000)
 
 TCPPlayerSocket = socket.socket(
@@ -21,33 +17,35 @@ TCPPlayerSocket = socket.socket(
 )  # create TCP welcoming socket
 
 
-def producerTask(queue):
-    numberOfSegments = linecache.getline(
-        "manifest.txt", 7
-    ).strip()  # Numero de segmentos
-    segmentNameLine = (
-        2 + track + ((int(numberOfSegments) + 4) * (track - 1))
-    )  # Line for the track name
-    segmentLinesBegin = 2 + (5 * track) + (int(numberOfSegments) * (track - 1))
+def producerTask(queue, baseURL, movieName, track):
     path = baseURL + movieName
     r = requests.get(path + "/manifest.txt")
-    with open("manifest.txt", "wb") as f:
+    with open("manifest.txt", "wb") as f:  # baixar manifest
         f.write(r.content)
+    numberOfSegments = linecache.getline(
+        "manifest.txt", 7
+    ).strip()  # Numero de segmentos do clip
+    segmentNameLine = (
+        2 + track + ((int(numberOfSegments) + 4) * (track - 1))
+    )  # Nome da track
+    segmentLinesBegin = (
+        2 + (5 * track) + (int(numberOfSegments) * (track - 1))
+    )  # Inicio do numero de segmentos
     segmentName = linecache.getline("manifest.txt", segmentNameLine).strip()
+    print(segmentName)
     for i in range(1, int(numberOfSegments) + 1):
-        segment = linecache.getline(
-            "manifest.txt", int(segmentLinesBegin) + i
-        ).strip()
+        segment = linecache.getline("manifest.txt", int(segmentLinesBegin) + i).strip()
         list = segment.split()
         beginAndEnd = [int(num) for num in list]
         begin = beginAndEnd[0]
-        end = beginAndEnd[1]
-        headers = {"Range": "bytes={}-{}".format(begin, end)}
+        end = beginAndEnd[1] + begin - 1
+        headers = {"Range": f"bytes={begin}-{end}"}
         r = requests.get(path + "/" + segmentName, headers=headers)
         queue.put(r.content)
         print("Segmento {} enviado!".format(i))
     queue.put(None)  # Fim do segmento
     print("Producer done!!")
+
 
 def consumerTask(queue):
     TCPPlayerSocket.connect(playerAddressPort)
@@ -64,15 +62,31 @@ def consumerTask(queue):
 
 
 def main():
+    if len(sys.argv) != 4:
+        print("Número inválido de argumentos!")
+        sys.exit(0)
+
+    baseURL = sys.argv[1]
+    movieName = sys.argv[2]
+    track = int(sys.argv[3])
+
     queue = Queue()
+
+    # Producer
+    producer = Thread(
+        target=producerTask,
+        args=(
+            queue,
+            baseURL,
+            movieName,
+            track,
+        ),
+    )
+    producer.start()
 
     # Consumer
     consumer = Thread(target=consumerTask, args=(queue,))
     consumer.start()
-
-    # Producer
-    producer = Thread(target=producerTask, args=(queue,))
-    producer.start()
 
     # Finish
     producer.join()
